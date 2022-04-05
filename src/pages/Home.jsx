29cf3792fb0faeb5160a17aa0530693eb0075c41
sebitza1020 <munteansebastian@gmail.com>
2022-04-05 20:15:44 +0300

    Popular and top movies&tv added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { OutlineButton } from "../components/button/Button";
 
+import { OutlineButton } from "../components/button/Button";
 import HeroSlide from "../components/hero-slide/HeroSlide";
+import MovieList from "../components/movie-list/MovieList";
+
+import { category, movieType, tvType } from "../api/tmdbApi";
 
 const Home = () => {
     return (
@@ -16,6 +19,37 @@ const Home = () => {
                             <OutlineButton classname="small">Vezi mai multe</OutlineButton>
                         </Link>
                     </div>
+                    <MovieList category={category.movie} type={movieType.popular}/>
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Filme De Top</h2>
+                        <Link to="/film">
+                            <OutlineButton classname="small">Vezi mai multe</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.movie} type={movieType.top_rated}/>
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Seriale Populare</h2>
+                        <Link to="/serial">
+                            <OutlineButton classname="small">Vezi mai multe</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.popular}/>
+                </div>
+
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Seriale De Top</h2>
+                        <Link to="/serial">
+                            <OutlineButton classname="small">Vezi mai multe</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.top_rated}/>
                 </div>
             </div>
         </>
