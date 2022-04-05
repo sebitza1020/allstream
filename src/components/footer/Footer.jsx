1fdc50cdccd2643a68e85e7a74a197fe51266307
sebitza1020 <munteansebastian@gmail.com>
2022-04-05 21:07:26 +0300

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                        <Link to="/">AllStream</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/">Acasă</Link>
+                        <Link to="/">Contactați-ne</Link>
+                        <Link to="/">Termeni și condiții</Link>
+                        <Link to="/">Despre noi</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">Live</Link>
+                        <Link to="/">FAQ</Link>
+                        <Link to="/">Premium</Link>
+                        <Link to="/">Politica de confidențialitate</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">Trebuie să urmărești</Link>
+                        <Link to="/">Titlu recent</Link>
+                        <Link to="/">Top IMDB</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
