329c24dae4f90fe28a24deaaaa5992ae770613ec
sebitza1020 <munteansebastian@gmail.com>
2022-04-05 19:44:52 +0300

    Homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/button/Button";
 
 import HeroSlide from "../components/hero-slide/HeroSlide";
 
@@ -6,6 +8,16 @@ const Home = () => {
     return (
         <>
             <HeroSlide/>
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Filme Populare</h2>
+                        <Link to="/film">
+                            <OutlineButton classname="small">Vezi mai multe</OutlineButton>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
