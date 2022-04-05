4a30a75558fe39574e6de247a180527d317d333a
sebitza1020 <munteansebastian@gmail.com>
2022-04-05 20:45:03 +0300

    Movie Card added.

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -11,6 +11,8 @@ import Button from '../button/Button';
 import tmdbApi, { category } from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 
+import MovieCard from '../movie-card/MovieCard';
+
 const MovieList = props => {
 
     const [items, setItems] = useState([]);
@@ -47,7 +49,7 @@ const MovieList = props => {
                 {
                     items.map((item, i) => (
                         <SwiperSlide key={i}>
-                            <img src={apiConfig.w500Image(item.poster_path)} alt="" />
+                            <MovieCard item={item} category={props.category}/>
                         </SwiperSlide>
                     ))
                 }
